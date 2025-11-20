c041011dcaabeb9837af33b924c4e1d7beeea465
Daniel Dominguez <daniel@veridise.com>
2025-11-20 13:49:14 +0100

    Fix macro

--- crates/support-macros/src/extractor.rs
+++ crates/support-macros/src/extractor.rs
@@ -10,9 +10,6 @@ pub fn derive_no_chip_args_impl(input: DeriveInput) -> TokenStream {
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     quote! {
-        impl #impl_generics extractor_support::circuit::NoChipArgs for #name #ty_generics
-        where
-            #where_clause
-        {}
+        impl #impl_generics extractor_support::circuit::NoChipArgs for #name #ty_generics #where_clause {}
     }
 }
