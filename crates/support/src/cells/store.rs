458a3a9ea1a0c6c0cc528badf9cca9e3bd0fe8e3
Daniel Dominguez <daniel@veridise.com>
2025-11-20 21:56:49 +0100

    Add convenience trait

--- crates/support/src/cells/store.rs
+++ crates/support/src/cells/store.rs
@@ -69,3 +69,99 @@ impl<
         self.1.store(ctx, chip, layouter, injected_ir)
     }
 }
+
+/// Helper trait for containers of [`StoreIntoCells`] implementations.
+///
+/// This trait does not require implementing [`CellReprSize`] so it's
+/// convenient for types such as Vec or Option.
+pub trait StoreIntoCellsDyn<F: Field, C, H: Halo2Types<F>, L> {
+    /// Stores an instance of Self into a set of cells.
+    fn store(
+        self,
+        ctx: &mut OCtx<F, H>,
+        chip: &C,
+        layouter: &mut impl LayoutAdaptor<F, H, Adaptee = L>,
+        injected_ir: &mut InjectedIR<H::RegionIndex, H::Expression>,
+    ) -> Result<(), H::Error>;
+}
+
+impl<T, F, C, H, L> StoreIntoCellsDyn<F, C, H, L> for T
+where
+    T: StoreIntoCells<F, C, H, L>,
+    F: Field,
+    H: Halo2Types<F>,
+{
+    fn store(
+        self,
+        ctx: &mut OCtx<F, H>,
+        chip: &C,
+        layouter: &mut impl LayoutAdaptor<F, H, Adaptee = L>,
+        injected_ir: &mut InjectedIR<
+            <H as Halo2Types<F>>::RegionIndex,
+            <H as Halo2Types<F>>::Expression,
+        >,
+    ) -> Result<(), <H as Halo2Types<F>>::Error> {
+        self.store(ctx, chip, layouter, injected_ir)
+    }
+}
+
+impl<T, F, C, H, L> StoreIntoCellsDyn<F, C, H, L> for Vec<T>
+where
+    T: StoreIntoCellsDyn<F, C, H, L>,
+    F: Field,
+    H: Halo2Types<F>,
+{
+    fn store(
+        self,
+        ctx: &mut OCtx<F, H>,
+        chip: &C,
+        layouter: &mut impl LayoutAdaptor<F, H, Adaptee = L>,
+        injected_ir: &mut InjectedIR<
+            <H as Halo2Types<F>>::RegionIndex,
+            <H as Halo2Types<F>>::Expression,
+        >,
+    ) -> Result<(), <H as Halo2Types<F>>::Error> {
+        self.into_iter().try_for_each(|e| e.store(ctx, chip, layouter, injected_ir))
+    }
+}
+
+impl<T, F, C, H, L> StoreIntoCellsDyn<F, C, H, L> for Option<T>
+where
+    T: StoreIntoCellsDyn<F, C, H, L>,
+    F: Field,
+    H: Halo2Types<F>,
+{
+    fn store(
+        self,
+        ctx: &mut OCtx<F, H>,
+        chip: &C,
+        layouter: &mut impl LayoutAdaptor<F, H, Adaptee = L>,
+        injected_ir: &mut InjectedIR<
+            <H as Halo2Types<F>>::RegionIndex,
+            <H as Halo2Types<F>>::Expression,
+        >,
+    ) -> Result<(), <H as Halo2Types<F>>::Error> {
+        self.into_iter().try_for_each(|e| e.store(ctx, chip, layouter, injected_ir))
+    }
+}
+
+impl<T, E, F, C, H, L> StoreIntoCellsDyn<F, C, H, L> for Result<T, E>
+where
+    T: StoreIntoCellsDyn<F, C, H, L>,
+    F: Field,
+    H: Halo2Types<F>,
+    H::Error: From<E>,
+{
+    fn store(
+        self,
+        ctx: &mut OCtx<F, H>,
+        chip: &C,
+        layouter: &mut impl LayoutAdaptor<F, H, Adaptee = L>,
+        injected_ir: &mut InjectedIR<
+            <H as Halo2Types<F>>::RegionIndex,
+            <H as Halo2Types<F>>::Expression,
+        >,
+    ) -> Result<(), <H as Halo2Types<F>>::Error> {
+        self?.store(ctx, chip, layouter, injected_ir)
+    }
+}
