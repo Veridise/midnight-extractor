4e8e8056f8a5b28dad58c001f954cd6557e2f32b
0xddom <0xddom@users.noreply.github.com>
2025-12-12 16:52:03 +0000

    deploy: bf66750c0610648ca371163ee61ef1431a51764d

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-eb7e533e.js',
+                path_to_root + 'searchindex-539e258e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
